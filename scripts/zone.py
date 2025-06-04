a922400973c8befcc3d09813feba9caa287bba07
swe-sys <sgjain222@gmail.com>
2025-06-04 21:07:18 +0530

    Created lattice package

--- scripts/zone.py
+++ scripts/zone.py
@@ -1,202 +1,159 @@
 #!/usr/bin/env python3
-import rospy 
-from geometry_msgs.msg import Point, Twist, Pose2D
-from sensor_msgs.msg import LaserScan
+
+import rospy
 import numpy as np
-from collections import deque
-from math import *
-from swarm_aggregation.msg import obs
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
 from nav_msgs.msg import Odometry
-import matplotlib.pyplot as plt
+from tf.transformations import euler_from_quaternion
+from math import atan2, sqrt, pi
+from sensor_msgs.msg import LaserScan
+from swarm_aggregation.msg import botPose
+import rospkg
+import os
 
-class obstacle(Pose2D):
-    def __init__(self,x,y,theta,dist,min_dis):
-        super().__init__(x,y,theta)
-        self.static = False
-        self.detected_time = rospy.get_time()
-        self.dist = dist
-        self.min_dis = min_dis
-    
-    def __eq__(self,other):
-        if abs(self.x - other.x)< 0.1 :
-            if abs(self.y - other.y)< 0.1 :
-                return True
-            else:
-                return False
-        else:
-            return False
-
-class robot:
-    def __init__(self,no_of_bots): 
-        self.total_bots = no_of_bots 
+class Robot(object):
+    def __init__(self, no_of_bots):
         self.x = 0
-        self.y = 0        
-        self.incident_time = []           
-        self.yaw = 0
-        self.range = [0]
-        self.time = 0
-        self.angle = 0
-        self.ang_max = 0
-        self.ang_inc = 0
-        self.disij = []
-        self.delij = []             
-        self.bearing = [0]
-        self.neigh = 0
-        self.robot = []
-        self.safe_zone = [0,0,0]
-        self.initial_no = -1                      
-        self.goal = Point(np.random.uniform(-6,6), np.random.uniform(-6,6), 0.0)
-        self.odom = Odometry()
-        self.namespace = rospy.get_namespace()
+        self.y = 0
+        self.cur_bot_id_indx = 0
+        self.total_bots = no_of_bots
         self.speed = Twist()
-        self.hist = deque(maxlen=20)
+        self.ranges = []
+        self.bot_odom = [Odometry() for _ in range(self.total_bots)]
+        self.neigh = []
+        self.goal = Point(np.random.uniform(-10,10), np.random.uniform(-10,10), 0.0)
+        self.x_bounds = (-10, 10)
+        self.y_bounds = (-10, 10)
+        self.detected_lattices = []
+        self.known_obstacles = []
+        self.lattice_spacing = 1.3
 
-        self.object_detector = rospy.Subscriber('/scan',LaserScan, self.scanner)
-        self.odom_sub = rospy.Subscriber("/odom",Odometry,self.update_Odom) 
+        self.namespace = rospy.get_namespace()
+        self.node_name = rospy.get_name()
+        self.odom = Odometry()
 
-        self.cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=1)    
-        self.pubg = rospy.Publisher('/goal', Point, queue_size=1)
-        self.rad_pub = rospy.Publisher('/radius', Point, queue_size=1)
-        self.obs_pub = rospy.Publisher('/obs',obs, queue_size=1)      
-        
-        
-    def update_Odom(self,odom):
-        """ Odometry of current bot"""        
-        self.x = odom.pose.pose.position.x 
-        self.y = odom.pose.pose.position.y
-        self.rot_q = odom.pose.pose.orientation
-        euler = euler_from_quaternion([self.rot_q.x , self.rot_q.y , self.rot_q.z , self.rot_q.w ])
-        self.yaw = atan2(sin(euler[2]),cos(euler[2])) 
-        self.odom = Pose2D(self.x,self.y,self.yaw)
-        self.vel = Pose2D(odom.twist.twist.linear.x,odom.twist.twist.linear.y,odom.twist.twist.angular.z)
+        self.botpose_sub = rospy.Subscriber('/obs_data', botPose, self.detect_bots)
+        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.update_odom)
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+        self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.goal_pub = rospy.Publisher('/goal', Point, queue_size=10)
 
-    def scanner(self,msg):       
-        self.range = msg.ranges
-        self.ang_max = msg.angle_max
-        self.ang_inc = msg.angle_increment
-        self.obs = []        
-        pairs = []
-        for i in range(len(self.range)):
-            if not isinf(self.range[i]):
-                j = i - 360
-                pairs.append([j,self.range[i]])
-        self.cones = self.split_list(pairs)
+    def update_odom(self, msg):
+        self.odom = msg
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        orientation_q = msg.pose.pose.orientation
+        _, _, yaw = euler_from_quaternion([orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
+        self.yaw = yaw % (2 * pi)
 
-        try:
-            for i in self.cones:
-                angle = np.mean(np.array(i)[:,0])*(pi/180)
-                distance = np.mean(np.array(i)[:,1])
-                min_dis = np.min(np.array(i)[:,1])
-                if distance < 3 and (-60*pi/180 < angle < 60*pi/180):
-                    obs_x = distance*cos(angle)
-                    obs_y = distance*sin(angle)
-                    global_x = self.x + (obs_x*cos(-self.yaw) + obs_y*sin(-self.yaw))
-                    global_y = self.y + (-obs_x*sin(-self.yaw) + obs_y*cos(-self.yaw))
-                    self.obs.append(obstacle(global_x,global_y,angle,distance,min_dis))
-                    print(self.obs, self.namespace, 'Obs')                           
-            self.hist.append([self.obs])
-        except (IndexError):
-            self.obs = []
+    def detect_bots(self,data):
+        """ Odometry of all bots"""
+        bot_id = data.bot_id
+        odoms = data.botpose
+        self.bot_odom = odoms
+        self.cur_bot_id_indx = bot_id.index(self.namespace)
 
-    def split_list(self,input_list):                                                                                                                                                                                                                                                                                                                
-        sublists = []
-        sublist = []
-        prev_degree = None
-        try:
-            for item in input_list:
-                degree, _ = item                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    def obstacle_detector(self, msg):
+        self.ranges = msg.ranges
 
-                if prev_degree is None or abs(degree - prev_degree) <= self.ang_inc*3:
-                    sublist.append(item)
-                else:
-                    sublists.append(sublist)
-                    sublist = [item]
-                prev_degree = degree
+    def is_obstacle_nearby(self, threshold=0.7):
+        return any(distance < threshold for distance in self.ranges if distance > 0)
 
-            sublists.append(sublist)
-        except (ValueError, TypeError) as e:
-            print("Error occurred while splitting the list:", str(e))
-
-        return sublists
-    
     def wall_following(self):
-        """funct to follow wall boundary
-        Turn Right by default or rotate on CCW fashion"""
-        # print("Wall following")
-        deg = 30
-        dst = 0.5
-        # while True:
-        if min(self.ranges[0:deg]) <= dst or min(self.ranges[(359-deg):]) <= dst: # front wall
+        deg, dst = 50, 0.7
+        if min(self.ranges[:deg]) <= dst or min(self.ranges[-deg:]) <= dst:
             self.speed.angular.z = -0.2
-            self.speed.linear.x = 0.0
-        elif min(self.ranges[deg:120]) < dst: # left wall 
+            self.speed.linear.x = 0.0                   
+        elif min(self.ranges[deg:120]) < dst:
             self.speed.angular.z = 0.0
             self.speed.linear.x = 0.2
-            # print("Left wall")
-        else:
-            self.speed.angular.z = 0.1
-            self.speed.linear.x = 0.2
-    
+
+    def distance(self, p1, p2):
+        return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+    def detect_new_obstacle(self, obs_pos):
+        self.known_obstacles.append(obs_pos)
+        for prev in self.known_obstacles[:-1]:
+            if abs(self.distance(obs_pos, prev) - self.lattice_spacing) < 0.2:
+                mid = ((obs_pos[0] + prev[0]) / 2, (obs_pos[1] + prev[1]) / 2)
+                dx, dy = obs_pos[0] - prev[0], obs_pos[1] - prev[1]
+                d = sqrt(dx**2 + dy**2)
+                nx, ny = -dy/d, dx/d
+                height = self.lattice_spacing * sqrt(3)/2
+                third1 = (mid[0] + nx * height, mid[1] + ny * height)
+                third2 = (mid[0] - nx * height, mid[1] - ny * height)
+                for pt in self.known_obstacles:
+                    if self.distance(pt, third1) < 0.2 or self.distance(pt, third2) < 0.2:
+                        self.detected_lattices.append([obs_pos, prev, pt])
+                        break
+
+    def is_point_near_lattice(self, point, threshold=1.0):
+        for tri in self.detected_lattices:
+            if any(self.distance(point, vertex) < threshold for vertex in tri):
+                return True
+        return False
+
     def set_goal(self):
-        """write code for identification based goal update of robot"""
-        """If robot -> update goal
-        If obstacle -> wall following"""
+        disij = [self.distance((odom.pose.pose.position.x, odom.pose.pose.position.y), (self.x, self.y))
+                 for odom in self.bot_odom]
+        delij = [atan2((odom.pose.pose.position.y - self.y), (odom.pose.pose.position.x - self.x))
+                 for odom in self.bot_odom]
+        self.neigh = [odom for i, odom in enumerate(self.bot_odom)
+                      if disij[i] <= 2 and disij[i] > 0.1 and -2.1 <= delij[i] <= 2.1]
+        self.neigh.append(self.odom)
 
-    def controller(self,k):
+        if len(self.neigh) >= 2:            
+            self.goal.x = np.mean([odom.pose.pose.position.x for odom in self.neigh])
+            self.goal.y = np.mean([odom.pose.pose.position.y for odom in self.neigh])
+        else:
+            while True:
+                gx = np.random.uniform(self.x_bounds[0], self.x_bounds[1])
+                gy = np.random.uniform(self.y_bounds[0], self.y_bounds[1])
+                if not self.is_point_near_lattice((gx, gy)):
+                    self.goal = Point(gx, gy, 0.0)
+                    break
+
+    def control(self):
         self.set_goal()
-        self.incx = (self.goal.x - self.x)
-        self.incy = (self.goal.y - self.y)
+        incx = self.goal.x - self.x
+        incy = self.goal.y - self.y
+        self.bearing = atan2(incy, incx)
+        self.dis_err = sqrt(incx**2 + incy**2)
+        self.dtheta = self.bearing - self.yaw
+        K = 0.3        
 
-        # Bearing of bot
-        self.bearing.append(atan2(self.incy,self.incx))
-
-        # Distance Error
-        self.dis_err = (sqrt(self.incx**2+self.incy**2))
-        #print(self.dis_err)        
-
-        # Gradient of Bearing
-        self.dtheta = (self.bearing[k] - self.bearing[k-1])/h
-
-        # for obs_element in self.obs:
-        #     x_diff = obs_element.x -self.x
-        #     y_diff = obs_element.y -self.y
-            
-        #     dist = sqrt(x_diff**2 + y_diff**2)
-        #     ang = atan2(y_diff, x_diff)
-            
-        #     self.disij.append(dist)
-        #     self.delij.append(ang)  
-
-        if (self.dis_err) >= 0.850:
-            """write code to control movement of robots based on conditions satisfied"""
-            """No obstacle detected -> 
+        if self.dis_err >= 0.885:
+            if len(self.disij) == 0:
                 self.speed.linear.x = 0.18
-                self.speed.angular.z = K*np.sign(self.dtheta)
-                
-                Robot Near -> t = rospy.get_time()
-                        self.speed.linear.x = max((0.18 -(5000-t)*0.0001),0)                    
-                        self.speed.angular.z = K*np.sign(self.dtheta)- 0.866*np.sign(self.delij[i])"""
-            
-        self.speed.linear.x = 0.18
-        self.speed.angular.z = K*np.sign(self.dtheta)
+                self.speed.angular.z = K * np.sign(self.dtheta)
+            elif len(self.neigh) == 0 and (min(self.ranges[0:30]) <= 0.7 or min(self.ranges[330:360]) <= 0.7):
+                self.wall_following()
+            else:
+                for i, z in enumerate(self.disij):
+                    if z >= 0.7:
+                        self.speed.linear.x = 0.18
+                        self.speed.angular.z = K * np.sign(self.dtheta)
+                    else:
+                        t = rospy.get_time()
+                        self.speed.linear.x = max((0.18 - (5000 - t) * 0.0001), 0)
+                        self.speed.angular.z = K * np.sign(self.dtheta) - 0.866 * np.sign(self.delij[i])
+        else:
+            if len(self.neigh) >= 2:
+                self.speed.linear.x = 0.0
+                self.speed.angular.z = 0.0
+                rospy.loginfo(f"Aggregated: {self.namespace}")
+            else:
+                self.goal = Point(3.0, -2.0, 0.0)
 
-        self.cmd_vel.publish(self.speed)
-        self.pubg.publish(self.goal)
+        self.cmd_vel_pub.publish(self.speed)
+        self.goal_pub.publish(self.goal)
 
 if __name__ == '__main__':
-    rospy.init_node("obstacle_controller")
-    rospy.loginfo("Chal Gye badde")
-    k = 0
-    l = [] #l is time
+    rospy.init_node("swarm_controller")
     rate = rospy.Rate(4)
-    bot = robot(6)     
+    bot = Robot(no_of_bots=6)
     rospy.sleep(6)
-    # bot.set_goal()
-    while not rospy.is_shutdown() and k < 500000:
-        k = k+1
-        h = 0.25
-        K = 0.3
-        l.append((k+1)/10) # Time
-        bot.controller(k)            
+
+    while not rospy.is_shutdown():
+        bot.control()
         rate.sleep()
